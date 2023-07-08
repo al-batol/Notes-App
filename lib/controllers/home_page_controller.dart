e826ff9d544c07dc290e626dd726db793de9d712
mohammed <mohammed032018@gmail.com>
2023-07-08 23:52:36 +0300

    saving sorted list

--- lib/controllers/home_page_controller.dart
+++ lib/controllers/home_page_controller.dart
@@ -43,7 +43,6 @@ class HomePageController extends GetxController {
   bool get onDraggable => _onDraggable;
   bool isLoaded = false;
 
-
   @override
   void dispose() async {
     titleEditing.dispose();
@@ -62,7 +61,7 @@ class HomePageController extends GetxController {
       bgColor = LightModeEdit.bgColor;
       titleAndButtonsColor = LightModeEdit.titleAndButtonsColor;
       textColor = LightModeEdit.textColor;
-    } else if(themeMode == ThemeMode.dark) {
+    } else if (themeMode == ThemeMode.dark) {
       appBarAndPageColor = DarkModeEdit.appBarAndPageColor;
       bgColor = DarkModeEdit.bgColor;
       titleAndButtonsColor = DarkModeEdit.titleAndButtonsColor;
@@ -74,8 +73,23 @@ class HomePageController extends GetxController {
     List<Map<String, dynamic>> allNotes =
         await appRepo.readData(SqlQueries.readDataSql());
     _notes.addAll(allNotes.map((e) => NoteModel.fromJson(e)).toList());
+    await getSortedData();
     calculatePageSize(AppDimensions.fontSize);
   }
+
+  Future<void> getSortedData() async {
+    if (await appRepo.getData('sort') == 0) {
+      appRepo.setData('sort', _sort == sortBy[0] ? 1 : 2);
+    } else {
+      _sort = await appRepo.getData('sort') == 1 ? sortBy[0] : sortBy[1];
+      if(await appRepo.getData('sort') == 1 ) {
+        sortNotes(_sort);
+      } else if(await appRepo.getData('sort') == 2) {
+        sortNotes(_sort);
+      }
+    }
+  }
+
   int notesNumber = 1;
   Future<void> insertData(String title, String topic) async {
     DateFormat format = DateFormat('MMM d, yyyy');
@@ -88,7 +102,11 @@ class HomePageController extends GetxController {
       'timestamp': timeStamp.millisecondsSinceEpoch.toString(),
       'formattedTime': formattedTime,
     };
-    _notes.add(NoteModel.fromJson(note));
+    if (_sort == sortBy[0]) {
+      _notes.add(NoteModel.fromJson(note));
+    } else {
+      _notes.insert(0, NoteModel.fromJson(note));
+    }
     await appRepo.insertData(SqlQueries.insertDataSql(
         title, topic, timeStamp.millisecondsSinceEpoch, formattedTime));
     notesNumber = (NoteModel.fromJson(note).id! + 1);
@@ -132,18 +150,17 @@ class HomePageController extends GetxController {
         final int fillLines = lines - topicEditing.text.split('\n').length;
         topicEditing.text += '\n' * fillLines;
       }
-    }
-    else {
+    } else {
       topicEditing.text = '\n' * lines;
-      topicEditing.selection = TextSelection.fromPosition(
-          const TextPosition(offset: 0));
+      topicEditing.selection =
+          TextSelection.fromPosition(const TextPosition(offset: 0));
     }
   }
 
-  final List<String> items = ['S', 'LM', 'DM','Se', 'CL'];
+  final List<String> items = ['S', 'LM', 'DM', 'Se', 'CL'];
 
-  final List<String> sortBy = ['Asc', 'Des'];
-  String _sort = 'Asc';
+  final List<String> sortBy = ['ASC', 'DESC'];
+  String _sort = 'ASC';
 
   String get sort {
     return _sort;
@@ -151,6 +168,7 @@ class HomePageController extends GetxController {
 
   set sort(String value) {
     _sort = value;
+    appRepo.setData('sort', value == sortBy[0] ? 1 : 2);
     update();
   }
 
@@ -158,29 +176,36 @@ class HomePageController extends GetxController {
   bool isOrdered = false;
 
   final List<String> languages = ['en', 'ar'];
-  // String _sort = '';
 
   // sort
   void sortNotes(String sortType) {
     if (sortType == sortBy[0] && isOrdered) {
-      isOrdered = false;
-      _notes.sort((a, b) {
-        DateTime timeA =
-            DateTime.fromMicrosecondsSinceEpoch(int.parse(a.timeStamp!));
-        DateTime timeB =
-            DateTime.fromMicrosecondsSinceEpoch(int.parse(b.timeStamp!));
-        return timeA.compareTo(timeB);
-      });
+      sortAsc();
     } else if (sortType == sortBy[1] && !isOrdered) {
-      isOrdered = true;
-      _notes.sort((a, b) {
-        DateTime timeA =
-            DateTime.fromMillisecondsSinceEpoch(int.parse(a.timeStamp!));
-        DateTime timeB =
-            DateTime.fromMillisecondsSinceEpoch(int.parse(b.timeStamp!));
-        return timeB.compareTo(timeA);
-      });
+      sortDesc();
     }
+  }
+
+  void sortAsc() {
+    isOrdered = false;
+    _notes.sort((a, b) {
+      DateTime timeA =
+          DateTime.fromMicrosecondsSinceEpoch(int.parse(a.timeStamp!));
+      DateTime timeB =
+          DateTime.fromMicrosecondsSinceEpoch(int.parse(b.timeStamp!));
+      return timeA.compareTo(timeB);
+    });
+  }
+
+  void sortDesc() {
+    isOrdered = true;
+    _notes.sort((a, b) {
+      DateTime timeA =
+          DateTime.fromMillisecondsSinceEpoch(int.parse(a.timeStamp!));
+      DateTime timeB =
+          DateTime.fromMillisecondsSinceEpoch(int.parse(b.timeStamp!));
+      return timeB.compareTo(timeA);
+    });
   }
 
   // slider to change font size
@@ -225,8 +250,9 @@ class HomePageController extends GetxController {
     update();
   }
 
-  void saveColors(double value, int color, String part, ThemeMode themeMode) async {
-    if(themeMode == ThemeMode.light) {
+  void saveColors(
+      double value, int color, String part, ThemeMode themeMode) async {
+    if (themeMode == ThemeMode.light) {
       if (part == appParts[0]) {
         await appRepo.setData('appBarAndPageColorLight', color);
       } else if (part == appParts[1]) {
@@ -236,7 +262,7 @@ class HomePageController extends GetxController {
       } else if (part == appParts[3]) {
         await appRepo.setData('textColorLight', color);
       }
-    } else if(themeMode == ThemeMode.dark) {
+    } else if (themeMode == ThemeMode.dark) {
       if (part == appParts[0]) {
         await appRepo.setData('appBarAndPageColorDark', color);
       } else if (part == appParts[1]) {
@@ -249,9 +275,10 @@ class HomePageController extends GetxController {
     }
   }
 
-  void changeAppBarColor(double value, int appBarColorL, int appBarColorD, {ThemeMode? themeMode, bool willRest=false}) {
+  void changeAppBarColor(double value, int appBarColorL, int appBarColorD,
+      {ThemeMode? themeMode, bool willRest = false}) {
     hue = value;
-    if(themeMode == ThemeMode.light || willRest) {
+    if (themeMode == ThemeMode.light || willRest) {
       LightModeEdit.appBarAndPageColor = appBarColorL;
       Themes.customLightMode = Themes.customLightMode.copyWith(
         primaryColor: Color(appBarColorL),
@@ -260,26 +287,28 @@ class HomePageController extends GetxController {
             .copyWith(backgroundColor: Color(appBarColorL)),
       );
     }
-    if(themeMode == ThemeMode.dark || willRest) {
+    if (themeMode == ThemeMode.dark || willRest) {
       DarkModeEdit.appBarAndPageColor = appBarColorD;
       Themes.customDarkMode = Themes.customDarkMode.copyWith(
         primaryColor: Color(appBarColorD),
-        floatingActionButtonTheme: Themes.customDarkMode.floatingActionButtonTheme
+        floatingActionButtonTheme: Themes
+            .customDarkMode.floatingActionButtonTheme
             .copyWith(backgroundColor: Color(appBarColorD)),
       );
     }
     update();
   }
 
-  void changeBgColor(double value, int bgColorL, int bgColorD, {ThemeMode? themeMode,bool willRest=false}) {
+  void changeBgColor(double value, int bgColorL, int bgColorD,
+      {ThemeMode? themeMode, bool willRest = false}) {
     hue = value;
-    if(themeMode == ThemeMode.light || willRest) {
+    if (themeMode == ThemeMode.light || willRest) {
       LightModeEdit.bgColor = bgColorL;
       Themes.customLightMode = Themes.customLightMode.copyWith(
         scaffoldBackgroundColor: Color(bgColorL),
       );
     }
-    if(themeMode == ThemeMode.dark || willRest) {
+    if (themeMode == ThemeMode.dark || willRest) {
       DarkModeEdit.bgColor = bgColorD;
       Themes.customDarkMode = Themes.customDarkMode.copyWith(
         scaffoldBackgroundColor: Color(bgColorD),
@@ -289,9 +318,10 @@ class HomePageController extends GetxController {
   }
 
   void changeTitleAndButtonColor(
-      double value, int titleButtonColorL, int titleButtonColorD, {ThemeMode? themeMode,bool willRest=false}) {
+      double value, int titleButtonColorL, int titleButtonColorD,
+      {ThemeMode? themeMode, bool willRest = false}) {
     hue = value;
-    if(themeMode == ThemeMode.light || willRest) {
+    if (themeMode == ThemeMode.light || willRest) {
       LightModeEdit.titleAndButtonsColor = titleButtonColorL;
       Themes.customLightMode = Themes.customLightMode.copyWith(
         iconTheme: Themes.customLightMode.iconTheme
@@ -304,7 +334,7 @@ class HomePageController extends GetxController {
         ),
       );
     }
-    if(themeMode == ThemeMode.dark || willRest) {
+    if (themeMode == ThemeMode.dark || willRest) {
       DarkModeEdit.titleAndButtonsColor = titleButtonColorD;
       Themes.customDarkMode = Themes.customDarkMode.copyWith(
         iconTheme: Themes.customDarkMode.iconTheme
@@ -320,35 +350,36 @@ class HomePageController extends GetxController {
     update();
   }
 
-  void changeTextColor(double value, int textColorL, int textColorD, {ThemeMode? themeMode,bool willRest=false}) {
+  void changeTextColor(double value, int textColorL, int textColorD,
+      {ThemeMode? themeMode, bool willRest = false}) {
     hue = value;
-    if(themeMode == ThemeMode.light || willRest) {
+    if (themeMode == ThemeMode.light || willRest) {
       LightModeEdit.textColor = textColorL;
       Themes.customLightMode = Themes.customLightMode.copyWith(
           textTheme: Themes.customLightMode.textTheme.copyWith(
-            titleMedium: Themes.customLightMode.textTheme.titleMedium!
-                .copyWith(color: Color(textColorL)),
-            headlineMedium: Themes.customLightMode.textTheme.headlineMedium!
-                .copyWith(color: Color(textColorL)),
-            bodyLarge: Themes.customLightMode.textTheme.bodyLarge!
-                .copyWith(color: Color(textColorL)),
-            displayLarge: Themes.customLightMode.textTheme.displayLarge!
-                .copyWith(color: Color(textColorL)),
-          ));
+        titleMedium: Themes.customLightMode.textTheme.titleMedium!
+            .copyWith(color: Color(textColorL)),
+        headlineMedium: Themes.customLightMode.textTheme.headlineMedium!
+            .copyWith(color: Color(textColorL)),
+        bodyLarge: Themes.customLightMode.textTheme.bodyLarge!
+            .copyWith(color: Color(textColorL)),
+        displayLarge: Themes.customLightMode.textTheme.displayLarge!
+            .copyWith(color: Color(textColorL)),
+      ));
     }
-    if(themeMode == ThemeMode.dark || willRest) {
+    if (themeMode == ThemeMode.dark || willRest) {
       DarkModeEdit.textColor = textColorD;
       Themes.customDarkMode = Themes.customDarkMode.copyWith(
           textTheme: Themes.customDarkMode.textTheme.copyWith(
-            titleMedium: Themes.customDarkMode.textTheme.titleMedium!
-                .copyWith(color: Color(textColorD)),
-            headlineMedium: Themes.customDarkMode.textTheme.headlineMedium!
-                .copyWith(color: Color(textColorD)),
-            bodyLarge: Themes.customDarkMode.textTheme.bodyLarge!
-                .copyWith(color: Color(textColorD)),
-            displayLarge: Themes.customDarkMode.textTheme.displayLarge!
-                .copyWith(color: Color(textColorD)),
-          ));
+        titleMedium: Themes.customDarkMode.textTheme.titleMedium!
+            .copyWith(color: Color(textColorD)),
+        headlineMedium: Themes.customDarkMode.textTheme.headlineMedium!
+            .copyWith(color: Color(textColorD)),
+        bodyLarge: Themes.customDarkMode.textTheme.bodyLarge!
+            .copyWith(color: Color(textColorD)),
+        displayLarge: Themes.customDarkMode.textTheme.displayLarge!
+            .copyWith(color: Color(textColorD)),
+      ));
     }
     update();
   }
